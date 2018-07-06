b1d5f3d57abefe7d031412b7d10073f4772a8e54
LuK1337 <priv.luk@gmail.com>
2018-07-06 14:53:18 +0200

    sm8150-common: Override vbmeta version in kernel cmdline
    Bootloader stops reporting everything about vbmeta after
    flashing a disabled image, so we need to set the version
    of it here to ensure runtime vs. compatibility matrix
    check won't fail on OTA.
    
    Change-Id: Ia26a84cb38e2f03025809a3ec52587481a6d8217

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -35,6 +35,7 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.vbmeta.avb_version=1.0
 BOARD_KERNEL_IMAGE_NAME := Image-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
