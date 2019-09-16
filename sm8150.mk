1dcfdee2331e829b7083167eb529de0dcf6531ab
kelsiraed <dumbandroid@gmail.com>
2019-09-16 11:39:30 -0700

    audio

--- sm8150.mk
+++ sm8150.mk
@@ -30,8 +30,6 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     android.hardware.audio@5.0 \
     android.hardware.audio.common@5.0 \
