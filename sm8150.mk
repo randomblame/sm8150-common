fddb3169713f53768862b95c2a7e3e469cdda491
kelsi <dumbandroid@gmail.com>
2019-09-21 20:25:25 -0700

    Revert "audio and wifi revert changes"
    This reverts commit 8c2ac384cdf4b999d456947e86c05c3b98063bc2.

--- sm8150.mk
+++ sm8150.mk
@@ -30,9 +30,13 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.audio@5.0 \
+    android.hardware.audio.common@5.0 \
+    android.hardware.audio.common@5.0-util \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@5.0 \
+    android.hardware.audio.effect@5.0-impl
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
